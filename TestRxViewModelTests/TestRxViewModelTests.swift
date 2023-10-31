e2d69f25734610634ec7d96c6238b2dfcc8d0cb1
Jim <jim402204@gmail.com>
2023-10-31 18:03:19 +0800

    修正TestRxViewModelTests測試失敗

--- TestRxViewModelTests/TestRxViewModelTests.swift
+++ TestRxViewModelTests/TestRxViewModelTests.swift
@@ -22,48 +22,38 @@ final class TestRxViewModelTests: XCTestCase {
     
     var sut: LoginViewModel!
     
-//    var scheduler: TestScheduler!
+    var scheduler: TestScheduler!
     var bag: DisposeBag!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        sut = LoginViewModel(api: APIStub())
         bag = DisposeBag()
-//        scheduler = TestScheduler(initialClock: 0)
+        scheduler = TestScheduler(initialClock: 0, resolution: 0.1)
+        sut = LoginViewModel(api: APIStub(), scheduler: scheduler)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         sut = nil
         bag = nil
-//        scheduler = nil
+        scheduler = nil
     }
 
     func test1() {
-        
-//        let toastObserver = scheduler.createObserver((type: InfoType,text: String).self)
-//        sut.output.toast.bind(to: toastObserver).disposed(by: bag)
         
         sut.input.email.accept("jim402204@gmail.com")
         sut.input.password.accept("402204jim")
         
         XCTAssert(sut.output.buttonIsEncable.value)
         
-        let item = BehaviorRelay<(type: InfoType,text: String)?>(value: nil)
+        let item = BehaviorRelay<ToastInfo?>(value: nil)
+        sut.output.toastInfo.bind(to: item).disposed(by: bag)
 
-        sut.output.toast
-            .bind(to: item)
-            .disposed(by: bag)
-
-        sut.input.tapButton.accept(())
-
+        self.sut.input.tapButton.accept(())
+        scheduler.start()
+        
         dump(item.value)
-        XCTAssert(item.value! == (.info,"登入成功！"))
-//        XCTAssert(item.value == (.error,"信箱格式錯誤，請重新輸入"))
-        
-        
-//        XCTAssertTrue(toastObserver.events[0].value.element! == (.info, "登入成功！"))
-//        dump(toastObserver.events[0].value.element)
+        XCTAssert(item.value! == ToastInfo(type: .info,text: "登入成功！"))
     }
     
     func test2() {
@@ -73,14 +63,14 @@ final class TestRxViewModelTests: XCTestCase {
         
         XCTAssert(sut.output.buttonIsEncable.value)
         
-        let item = BehaviorRelay<(type: InfoType,text: String)?>(value: nil)
-        sut.output.toast.bind(to: item).disposed(by: bag)
+        let item = BehaviorRelay<ToastInfo?>(value: nil)
+        sut.output.toastInfo.bind(to: item).disposed(by: bag)
 
         sut.input.tapButton.accept(())
+        scheduler.start()
 
         dump(item.value)
-        
-        XCTAssert(item.value! == (.error,"信箱格式錯誤，請重新輸入"))
+        XCTAssert(item.value! == ToastInfo(type: .error,text: "信箱格式錯誤，請重新輸入"))
     }
     
     func test3() {
@@ -90,14 +80,14 @@ final class TestRxViewModelTests: XCTestCase {
         
         XCTAssert(sut.output.buttonIsEncable.value)
         
-        let item = BehaviorRelay<(type: InfoType,text: String)?>(value: nil)
-        sut.output.toast.bind(to: item).disposed(by: bag)
+        let item = BehaviorRelay<ToastInfo?>(value: nil)
+        sut.output.toastInfo.bind(to: item).disposed(by: bag)
 
         sut.input.tapButton.accept(())
+        scheduler.start()
 
         dump(item.value)
-        
-        XCTAssert(item.value! == (.error,"密碼格式錯誤，請重新輸入"))
+        XCTAssert(item.value! == ToastInfo(type: .error,text: "密碼格式錯誤，請重新輸入"))
     }
 
     func test4() {
@@ -106,14 +96,14 @@ final class TestRxViewModelTests: XCTestCase {
         sut.input.password.accept("")
         
         XCTAssertFalse(sut.output.buttonIsEncable.value)
+        scheduler.start()
         
-        let item = BehaviorRelay<(type: InfoType,text: String)?>(value: nil)
-        sut.output.toast.bind(to: item).disposed(by: bag)
+        let item = BehaviorRelay<ToastInfo?>(value: nil)
+        sut.output.toastInfo.bind(to: item).disposed(by: bag)
 
         sut.input.tapButton.accept(())
 
         dump(item.value)
-        
         XCTAssertNil(item.value)
     }
 
